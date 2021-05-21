4f41bc896ece850f173511b341ca14949f69bfdd
Onur Sinan Togrul <o.sinantogrul@azerion.com>
2021-05-21 03:28:10 +0300

    Added Ship turn rotation

--- Assets/arcade-galaxy/Scripts/Player/PlayerMovement.cs
+++ Assets/arcade-galaxy/Scripts/Player/PlayerMovement.cs
@@ -9,16 +9,59 @@ public class PlayerMovement : MonoBehaviour
     [Header("Attributes")]
     public float verticalMovementSpeed = 1f;
     public float horizontalMovementSpeed = 1f;
+    public float turnRotation;
+
+    [Header("Rockets")]
+    public ParticleSystem rocketLeft;
+    public ParticleSystem rocketRight;
+
+
+    private void Awake()
+    {
+        turnRotation = Mathf.Clamp(turnRotation, 0, turnRotation);
+    }
+    private void PlayRockets()
+    { 
+        rocketLeft.Play();
+        rocketRight.Play();
+    }
+
+    private void StopRockets()
+    {
+        rocketRight.Stop();
+        rocketLeft.Stop();
+    }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.A))
+        { 
             transform.Translate(Vector3.left * horizontalMovementSpeed, Space.World);
+            
+        }
         if (Input.GetKey(KeyCode.D))
+        { 
             transform.Translate(Vector3.right * horizontalMovementSpeed, Space.World);
+        }
         if (Input.GetKey(KeyCode.W))
             transform.Translate(Vector3.up * verticalMovementSpeed, Space.World);
         if (Input.GetKey(KeyCode.S))
             transform.Translate(Vector3.down * verticalMovementSpeed, Space.World);
+
+        // Rockets Particles
+        if (Input.GetKeyDown(KeyCode.W))
+            PlayRockets();
+        if (Input.GetKeyUp(KeyCode.W))
+            StopRockets();
+
+        // Turn Rotations
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.rotation = Quaternion.Euler(0, turnRotation, 0);
+        if (Input.GetKeyUp(KeyCode.A))
+            transform.rotation = Quaternion.identity;
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.rotation = Quaternion.Euler(0, -turnRotation, 0);
+        if (Input.GetKeyUp(KeyCode.D))
+            transform.rotation = Quaternion.identity;
     }
 }
