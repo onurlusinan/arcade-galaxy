59a9d4cd570362365f53f45ffe875af5a4b0c4a6
Onur Sinan Togrul <o.sinantogrul@azerion.com>
2021-05-22 16:51:18 +0300

    Rigidbodies added to alien ships, first collision tests

--- Assets/arcade-galaxy/Scripts/Characters/EnemyMovement.cs
+++ Assets/arcade-galaxy/Scripts/Characters/EnemyMovement.cs
@@ -2,24 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+namespace ArcadeGalaxy.Characters
 {
-    [Header("Enemy Follow Config")]
-    public float followSpeed;
-    public float distanceLimit;
-    
-    private Transform target;
-
-    private void Start()
+    public class EnemyMovement : MonoBehaviour
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();    
-    }
+        [Header("Enemy Follow Config")]
+        public float followSpeed;
+        public float distanceLimit;
 
-    private void Update()
-    {
-        if (Vector2.Distance(transform.position, target.position) > distanceLimit)
+        private Transform target;
+        private BoxCollider2D coll;
+
+        private void Start()
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, followSpeed * Time.deltaTime);
+            target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            coll = this.GetComponent<BoxCollider2D>();
+        }
+
+        void OnTriggerEnter2D(Collider2D coll)
+        {
+            if (coll.gameObject.tag == "Enemy")
+            {
+                Debug.Log("Enemy collides with Enemy!");
+            }
+            else if (coll.gameObject.tag == "Player")
+            {
+                Debug.Log("Enemy collides with Player!");
+            }
+        }
+
+        private void Update()
+        {
+            if (Vector2.Distance(transform.position, target.position) > distanceLimit)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, followSpeed * Time.deltaTime);
+            }
         }
     }
 }
