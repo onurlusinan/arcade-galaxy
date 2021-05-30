98f1aecc6d0ed1e1918b138f2dd84a5d205658a0
Onur Sinan Togrul <o.sinantogrul@azerion.com>
2021-05-30 20:49:41 +0300

    Create BulletManager and set as bullets parent

---
+++ Assets/arcade-galaxy/Scripts/Managers/BulletManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ArcadeGalaxy.Utilities;
+
+namespace ArcadeGalaxy.Managers 
+{ 
+    public class BulletManager : MonoBehaviour
+    {
+        private ObjectPool bulletPool;
+    }
+}
+
--- Assets/arcade-galaxy/Scripts/Weapon/Weapon.cs
+++ Assets/arcade-galaxy/Scripts/Weapon/Weapon.cs
@@ -7,11 +7,12 @@ namespace ArcadeGalaxy.Weapon
     public class Weapon : MonoBehaviour
     {
         public Transform firePoint;
+        public Transform bulletManager;
         public GameObject bullet;
 
         private void Shoot()
         {
-            Instantiate(bullet, firePoint.position, firePoint.rotation);
+            Instantiate(bullet, firePoint.position, firePoint.rotation, bulletManager);
         }
 
         void Update()
